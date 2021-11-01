b10029d13d0cd3ad780baa8b331cfbcb78443a76
ljusica <jessiicaceder@hotmail.com>
2021-11-01 14:31:48 +0100

    Added hella many pseudo-codes to enemy-script and added a shape

--- EYEgame/Assets/Scripts/EyeScript.cs
+++ EYEgame/Assets/Scripts/EyeScript.cs
@@ -4,15 +4,101 @@ using UnityEngine;
 
 public class EyeScript : MonoBehaviour
 {
+
+    public int x;
+    public int y;
+    public int direction;
+    bool collision = false;
+    bool fixedDirection = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+         * GetComponent.EyeHolder
+         * 
+         * Vector position = eyeGuard.x, eyeGuard.y
+         */
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /*
+         * OM eyeGuard.x == startPosition.x && eyeGuard.y == startPosition.y && fixedDirection == true
+         * {
+         *      eyeGuard.MoveRegularly();
+         * }
+         * 
+         * OM player kolliderar med sight
+         * {
+         *      eyeGuard.ChasePlayer();
+         *      collision = true;
+         * }
+         * 
+         * collision = false;
+         */
     }
+    /*
+     * Funktion : MoveRegularly
+     * {
+     *      fixedDirection = true;
+     *      public vector startPosition = this.position;
+     *      
+     *      OM direction == 1 && fixedDirection = true
+     *      {
+     *          Evighetsloop -
+     *          For-loop
+     *          Öka i x-axel visst antal steg * time.deltaTime
+     * 
+     *          For-loop
+     *          Minska i x-axel visst antal steg * time.deltaTime
+     *          - Slut på evighetsloop
+     *      }
+     * 
+     *      OM direction == 2 && fixedDirection = true
+     *      {
+     *          Evighetsloop -
+     *          For-loop
+     *          Öka i y-axel visst antal steg * time.deltaTime
+     * 
+     *          For-loop
+     *          Minska i y-axel visst antal steg * time.deltaTime
+     *          - Slut på evighetsloop
+     *      }
+     * 
+     *      ANNARS
+     *      {
+     *          deBug log = Please assign value to direction
+     *      }
+     *      
+     * }
+     * 
+     * 
+     * Funktion : ChasePlayer
+     * {
+     *      GetComponent<Player> = player;
+     *      GetComponent<Sight> = sight;
+     * 
+     *      do
+     *      {
+     *          fixedDirection = false;
+     *          sight.alpha = tonas upp och ned
+     *          player.hp--;
+     *          eyeGuard.x följer player.x
+     *          eyeGuard.y följer player.y
+     *      }   
+     *      while (collision == true + 5 sekunder)
+     *      
+     *      if collision = false
+     *      {
+     *          sight.alpha = 0.5f;
+     *                  
+     *          eyeGuard.x åker mot startPosition.x;
+     *          eyeGuard.y åker mot startPosition.y;
+     *      }
+     *      
+     * }
+    */
 }
+
