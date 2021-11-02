5afde23b7eab546ac806a40533bba3982e464018
ljusica <jessiicaceder@hotmail.com>
2021-11-02 16:30:33 +0100

    Removed some pseudo and added some ACTUAL code

--- EYEgame/Assets/Scripts/EyeScript.cs
+++ EYEgame/Assets/Scripts/EyeScript.cs
@@ -4,81 +4,87 @@ using UnityEngine;
 
 public class EyeScript : MonoBehaviour
 {
-
-    public int x;
-    public int y;
+    public float waitTime;
+    public float startWaitTime;
     public int direction;
+    public int speed = 5;
+    public int firstPoint;
+    public int secondPoint;
+    public Vector2 startPosition;
     public GameObject eyeGuard;
+    public Transform[] moveSpots;
     bool collision = false;
-    bool fixedDirection = false;
+    bool patrol = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        eyeGuard = GameObject.Find("EyeHolder");
 
-
-        /*
-         * GetComponent.EyeHolder
-         * 
-         * Vector position = eyeGuard.x, eyeGuard.y
-         */
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (patrol == true)
+        {
+            Patrol();
+        }
+
         /*
-         * OM eyeGuard.x == startPosition.x && eyeGuard.y == startPosition.y && fixedDirection == true
-         * {
-         *      eyeGuard.MoveRegularly();
-         * }
-         * 
          * OM player kolliderar med sight
          * {
-         *      eyeGuard.ChasePlayer();
+         *      ChasePlayer();
+         *      patrol = false;
          *      collision = true;
          * }
-         * 
-         * collision = false;
          */
     }
+
+    void Patrol()
+    {
+        startPosition = this.transform.position;
+        transform.position = moveSpots[0].position;
+        waitTime = startWaitTime;
+
+        while (patrol == true)
+        {
+            //if (direction == 1)
+            //{
+            transform.position = Vector2.MoveTowards(transform.position, moveSpots[firstPoint].position, speed * Time.deltaTime);
+
+            if (Vector2.Distance(transform.position, moveSpots[firstPoint].position) < 0.2f)
+            {
+                if (waitTime <= 0)
+                {
+                    waitTime = startWaitTime;
+                }
+
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+                //}
+            }
+
+            if (direction == 2)
+            {
+                //transform.position = transform.position + new Vector3(x, patrolDistance * speed * Time.deltaTime, 0);
+                //vänta och vänd?
+            }
+
+            else
+            {
+                Debug.Log("Please assign valid values to direction (1 = x or 2 = y) and plannedPoint");
+            }
+        }
+    }
+
+    void ChasePlayer()
+    {
+        patrol = false;
+    }
     /*
-     * Funktion : MoveRegularly
-     * {
-     *      fixedDirection = true;
-     *      public vector startPosition = this.position;
-     *      
-     *      OM direction == 1 && fixedDirection = true
-     *      {
-     *          Evighetsloop -
-     *          For-loop
-     *          Öka i x-axel visst antal steg * time.deltaTime
-     * 
-     *          For-loop
-     *          Minska i x-axel visst antal steg * time.deltaTime
-     *          - Slut på evighetsloop
-     *      }
-     * 
-     *      OM direction == 2 && fixedDirection = true
-     *      {
-     *          Evighetsloop -
-     *          For-loop
-     *          Öka i y-axel visst antal steg * time.deltaTime
-     * 
-     *          For-loop
-     *          Minska i y-axel visst antal steg * time.deltaTime
-     *          - Slut på evighetsloop
-     *      }
-     * 
-     *      ANNARS
-     *      {
-     *          deBug log = Please assign value to direction
-     *      }
-     *      
-     * }
-     * 
-     * 
+
      * Funktion : ChasePlayer
      * {
      *      GetComponent<Player> = player;
