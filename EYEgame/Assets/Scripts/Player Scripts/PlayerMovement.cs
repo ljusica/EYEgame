85e028b301bb0853a4f5e50e4eeec5420fdb3992
Jesper Johansson <33457842+imthatjeppe@users.noreply.github.com>
2021-11-04 12:01:38 +0100

    Revert "Added so you can't jump into walls and get stuck"
    This reverts commit 1fcb98f04a368d02f13b7664c2f78cbd8ed14ccb.

--- EYEgame/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ EYEgame/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -18,8 +18,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-        //healthbar.setMaxHealth(maxHealth);
-        Physics2D.queriesStartInColliders = false;
+        healthbar.setMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -27,36 +26,13 @@ public class PlayerMovement : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
 
-        Jump();
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            //rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
+            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
 
         movement.x = x * speed;
-
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 0.3f);
-        //Debug.DrawRay(transform.position, Vector2.right * 0.3f, Color.red, 0.2f);
-        if (hit.collider != null)
-        {
-            if (movement.x > 1)
-            {
-                movement.x = 0;
-            }
-        }
-
-        hit = Physics2D.Raycast(transform.position, Vector2.left, 0.3f);
-        //Debug.DrawRay(transform.position, Vector2.right * 0.3f, Color.red, 0.2f);
-        if (hit.collider != null)
-        {
-            if (movement.x < -1)
-            {
-                movement.x = 0;
-            }
-        }
-
-        hit = Physics2D.Raycast(transform.position, Vector2.down, 0.3f);
-        //Debug.DrawRay(transform.position, Vector2.right * 0.3f, Color.red, 0.2f);
-        if (hit.collider != null)
-        {
-            Jump();
-        }
 
         transform.rotation = Quaternion.identity;
 
@@ -65,17 +41,6 @@ public class PlayerMovement : MonoBehaviour
             TakeDamage(2);
         }
     }
-
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            //rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
-           
-            rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        }
-    }
-
     void FixedUpdate()
     {
         movement.y = rb2d.velocity.y;
@@ -89,15 +54,15 @@ public class PlayerMovement : MonoBehaviour
         healthbar.setHealth(currentHealth); 
     }
 
-    //private void OnTriggerEnter2D(Collider2D other)
-    //{
-    //    grounded = true;
-    //}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        grounded = true;
+    }
 
-    //private void OnTriggerExit2D(Collider2D other)
-    //{
-    //    grounded = false;
-    //}
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        grounded = false;
+    }
 
     public void StopMoving()
     {
