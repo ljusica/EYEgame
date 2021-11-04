838c7d1cb9bdf0f6d953658dd90555ac60233d75
Marcus Rundén <marcus.runden@hotmail.com>
2021-11-04 11:51:31 +0100

    preparations for barrels and grabbing key

---
+++ EYEgame/Assets/Scripts/Key.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+
+    private OnTriggerEnter(Collider other)
+    {
+
+    }
+}
